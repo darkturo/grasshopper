85722de1b5bddc5a4d6468237208f158159a4870
Arturo Escudero <darkturo@gmail.com>
2025-02-11 12:57:27 +0100

    Register auth blueprint
    Following the template here. Have chosen to use blueprints instead of
    registering all the routes in the app factory for readability,
    modularity, and also to play with something new

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -23,6 +23,10 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # Register authentication blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
