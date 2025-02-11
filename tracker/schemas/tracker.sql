5c3760cfa0c605cbfbf1fe43834f300e252a6436
Arturo Escudero <darkturo@gmail.com>
2025-02-11 13:09:10 +0100

    Add email to the user table

--- tracker/schemas/tracker.sql
+++ tracker/schemas/tracker.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS cpu_usage;
 
 CREATE TABLE users (
     id UUID PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
